0ece4fc9847a9401e2c20968c5f3a5be898868d9
Moin Ul Haq <moinfiverrmail@gmail.com>
2023-11-18 22:42:45 +0500

    program_4.py

--- program_4.py
+++ program_4.py
@@ -3,7 +3,7 @@ numbers_x = [10,20,30,40,10]
 numbers_y = [35,75,45,30,55]
 def list_check(list):
      print(f"Given List {list}")
-     if numbers_x[0] == numbers_x[-1]:
+     if list[0] == list[-1]:
         print("True")
 list_check(numbers_x)
 list_check(numbers_y)
