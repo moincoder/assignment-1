89d5b916b25c67c9cb913a354a4e2c0c71f689f6
Moin Ul Haq <moinfiverrmail@gmail.com>
2023-11-24 17:58:19 +0500

    Update program_2.py

--- program_2.py
+++ program_2.py
@@ -1,5 +1,4 @@
 #Print the sum of the current number and the previous number.
-current_number = 1
-for i in range(10):
-    previous_number = current_number-1
-    print(f"current number {current_number} previous number {previous_number} sum {current_number+previous_number}")
+for i in range(1,11):
+    previous_number = i-1
+    print(f"current number {i} previous number {previous_number} sum {i+previous_number}")
